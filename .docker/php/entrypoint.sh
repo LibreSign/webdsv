2741c256d6f66b671d86bc72f8e1a9a589925717
Vitor Mattos <vitor@php.rio>
2024-12-24 11:30:15 -0300

    chore: update docker files
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# Set uid of host machine
+usermod --non-unique --uid "${HOST_UID}" www-data
+groupmod --non-unique --gid "${HOST_GID}" www-data
+
 if [ ! -d "vendor" ]; then
     composer install
     chown -R www-data:www-data var
